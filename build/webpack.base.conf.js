17b79b7b2e72be75be85dae499efc7ca848d2ee2
koriyama <nekobato@gmail.com>
2016-03-29 19:20:55 +0900

    💩

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,69 @@
+var path = require('path')
+var cssLoaders = require('./css-loaders')
+var projectRoot = path.resolve(__dirname, '../')
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../app/dist/static'),
+    publicPath: './app/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    fallback: [path.join(__dirname, '../node_modules')],
+    alias: {
+      'src': path.resolve(__dirname, '../src')
+    }
+  },
+  resolveLoader: {
+    fallback: [path.join(__dirname, '../node_modules')]
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        include: projectRoot,
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: projectRoot,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.jade$/,
+        loader: 'jade'
+      },
+      {
+        test: /\.styl$/,
+        loader: "style!css!stylus"
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff2?|eot|ttf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ExternalsPlugin('commonjs', ['electron', 'screen', 'remote']),
+    new WebpackNotifierPlugin({title: 'Webpack'})
+  ],
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  }
+}
